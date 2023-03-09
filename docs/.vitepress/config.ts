41a6df370a53cf5493e2adcdf5568d2691e99da5
Jack <17665111266@163.com>
2023-03-09 09:04:31 +0800

    feat: 调整config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -57,12 +57,12 @@ export default defineConfig({
   description,
   head: [
     ['meta', { name: 'theme-color', content: '#ffffff' }],
-    ['link', { rel: 'icon', href: '/favicon.ico', type: 'image/png' }],
+    ['link', { rel: 'icon', href: '/public/favicon.ico', type: 'image/png' }],
     [
       'link',
       {
         rel: 'alternate icon',
-        href: '/favicon.ico',
+        href: '/public/favicon.ico',
         type: 'image/png',
         sizes: '16x16',
       },
